91bf37be4d4fa5a372b1c94ef9476498c1aff78c
Moshfegh Hamedani <moshfegh@live.com.au>
2017-01-22 12:49:24 +1100

    tassing.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,4 @@
+import { tassign } from 'tassign'; 
 import { INCREMENT } from './actions'; 
 
 export interface IAppState {
@@ -10,7 +11,9 @@ export const INITIAL_STATE: IAppState = {
 
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
-    case INCREMENT: return { counter: state.counter + 1 };
+    case INCREMENT: 
+      state.counter++;
+      return tassign(state, { counter: state.counter + 1 });
   }
   return state; 
 }
